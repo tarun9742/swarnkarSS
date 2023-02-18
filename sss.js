164a1a3345cf33f96c75a5c9d861bfc02aa0ecad
tarun9742 <109736993+tarun9742@users.noreply.github.com>
2023-02-18 10:55:40 +0530

    Add files via upload

---
+++ sss.js
@@ -0,0 +1,32 @@
+// greeting day message
+// const time = new Date().getHours();
+// let greeting;
+// if (time < 10) {
+//     greeting = "Good morning";
+// } else if (time < 20) {
+//     greeting = "Good day";
+// } else {
+//     greeting = "Good evening";
+// }
+// document.write(alert(greeting));
+
+function btn1function() {
+    alert("Our Committee members are : ");
+}
+
+function btn2function() {
+    // alert("If u want to access our student pannel you have to login first");
+    document.getElementById("btn2").src="https://www.google.co.in/";
+}
+
+function btn3function() {
+    alert("trying");
+}
+
+
+
+
+
+function morePhotos() {
+    
+}
